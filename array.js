e9003487ef5ede66a735cf47a0709f9c07104d79
WWcode Recife <karina@womenwhocode.com>
2016-09-28 18:28:22 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,49 @@
+/*****************************/
+
+        //Array.js - Meetup 1
+
+        // Women Who Code Recife
+
+ /*****************************/
+
+
+
+
+
+/*********** Crie um array chamado amigos.*************/
+
+// código aqui
+
+
+
+
+/*********** Adicione o primeiro nome do amigo no array na posiçāo 0 *************/
+
+// código aqui
+
+
+
+
+
+
+/*********** Adicione mais três nomes de amigos, [ "amigo1, amigo2,amigo3"]; *************/
+
+
+// código aqui
+
+
+
+
+
+/***********  Verifique o tamanho do array. *************/
+
+// código aqui
+
+
+
+
+
+/***********  Remova o último item do array. *************/
+
+// código aqui
+
